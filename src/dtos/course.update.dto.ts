e1febfdd93a9c618e1112dbf224c0c8d07c1448f
Manuel <jpol@fi.uba.ar>
2025-05-27 11:24:36 -0300

    fix: valid userId property for json body in PATCH course requestrefactor: made a script to wait the database in container to be ready before running the test.

--- src/dtos/course.update.dto.ts
+++ src/dtos/course.update.dto.ts
@@ -1,7 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CourseRequestDto } from './course.request.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CourseUpdateDto extends PartialType(CourseRequestDto) {
   /** Id of the user who generate the PATCH */
+  @IsNotEmpty()
+  @IsString()
   userId: string;
 }
