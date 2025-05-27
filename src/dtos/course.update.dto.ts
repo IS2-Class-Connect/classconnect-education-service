c6fdc6f214750ddf17b8fdbc6ec3e23ce07ddee1
Manuel <jpol@fi.uba.ar>
2025-05-27 10:28:46 -0300

    refactor: is no longer necessary to follow UUID format for userId

--- src/dtos/course.update.dto.ts
+++ src/dtos/course.update.dto.ts
@@ -1,9 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CourseRequestDto } from './course.request.dto';
-import { IsUUID } from 'class-validator';
 
 export class CourseUpdateDto extends PartialType(CourseRequestDto) {
   /** Id of the user who generate the PATCH */
-  @IsUUID()
   userId: string;
 }
