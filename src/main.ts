30240fe7a4355f08e123a6f55383150028698f1f
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-06-02 19:41:36 -0300

    /education/metrics

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,27 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './middleware/response.interceptor';
 import { BaseExceptionFilter } from './middleware/exception.filter';
+import { Request, Response, NextFunction } from 'express';
+import { requestCounter, responseTimeHistogram } from './controllers/metrics.controller';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
   );
+
+  // Middleware for prometheus metrics
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const end = responseTimeHistogram.startTimer();
+
+    res.on('finish', () => {
+      end({ method: req.method, route: req.path, status: res.statusCode.toString() });
+      requestCounter.inc({ method: req.method, route: req.path, status: res.statusCode.toString() });
+    });
+
+    next();
+  });
+
   app.useGlobalFilters(new BaseExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3002);
