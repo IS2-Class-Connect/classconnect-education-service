2e6e058f308c9036e2e93d8720f53051ec5acaab
mbianchif <89595589+mbianchif@users.noreply.github.com>
2025-06-05 13:22:00 -0300

    review changes:

--- src/main.ts
+++ src/main.ts
@@ -7,24 +7,38 @@ import { Request, Response, NextFunction } from 'express';
 import { cpuUsageGauge, memoryUsageGauge, requestCounter, responseTimeHistogram } from './controllers/metrics.controller';
 
 // Resource fetching for metrics
-let prevCpuUsage = process.cpuUsage();
-let prevHrTime = process.hrtime();
-setInterval(() => {
-  const currCpuUsage = process.cpuUsage(prevCpuUsage);
-  const currHrTime = process.hrtime(prevHrTime);
+function setupMetricsFetching() {
+  let prevCpuUsage = process.cpuUsage();
+  let prevHrTime = process.hrtime();
+  setInterval(() => {
+    const currCpuUsage = process.cpuUsage(prevCpuUsage);
+    const currHrTime = process.hrtime(prevHrTime);
 
-  prevCpuUsage = process.cpuUsage();
-  prevHrTime = process.hrtime();
+    prevCpuUsage = process.cpuUsage();
+    prevHrTime = process.hrtime();
 
-  const elapsedHrTimeSeconds = currHrTime[0] + currHrTime[1] / 1e9;
-  const totalCpuMicros = currCpuUsage.user + currCpuUsage.system;
+    const elapsedHrTimeSeconds = currHrTime[0] + currHrTime[1] / 1e9;
+    const totalCpuMicros = currCpuUsage.user + currCpuUsage.system;
 
-  const cpuPercent = (totalCpuMicros / 1e6 / elapsedHrTimeSeconds) * 100;
-  cpuUsageGauge.set(cpuPercent);
+    const cpuPercent = (totalCpuMicros / 1e6 / elapsedHrTimeSeconds) * 100;
+    cpuUsageGauge.set(cpuPercent);
 
-  const memoryUsage = process.memoryUsage();
-  memoryUsageGauge.set(memoryUsage.rss);
-}, 5000);
+    const memoryUsage = process.memoryUsage();
+    memoryUsageGauge.set(memoryUsage.rss);
+  }, 5000);
+}
+
+// Middleware for prometheus metrics
+function prometheusMiddleware(req: Request, res: Response, next: NextFunction) {
+  const end = responseTimeHistogram.startTimer();
+
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, status: res.statusCode.toString() });
+    requestCounter.inc({ method: req.method, route: req.path, status: res.statusCode.toString() });
+  });
+
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -32,18 +46,8 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
   );
 
-  // Middleware for prometheus metrics
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    const end = responseTimeHistogram.startTimer();
-
-    res.on('finish', () => {
-      end({ method: req.method, route: req.path, status: res.statusCode.toString() });
-      requestCounter.inc({ method: req.method, route: req.path, status: res.statusCode.toString() });
-    });
-
-    next();
-  });
-
+  setupMetricsFetching();
+  app.use(prometheusMiddleware);
   app.useGlobalFilters(new BaseExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3002);
