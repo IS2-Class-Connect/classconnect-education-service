784ddb5859e4742be0a3f79351f8c650c3b81658
Manuel Pol <90402724+Manuel-Pol@users.noreply.github.com>
2025-06-11 17:15:58 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,14 @@ import { MetricsModule } from './modules/metrics.module';
 import { AssessmentModule } from './modules/assessment.module';
 import { env } from 'process';
 
+const mongoUrl = env.MONGODB_URL ?? 'mongodb://cc_user:cc_password@localhost:27017';
+const mongoDb = env.MONGODB_NAME ?? 'classconnect?authSource=classconnect';
+
 @Module({
   imports: [
     PrismaModule,
     CourseModule,
-    MongooseModule.forRoot(
-      env.MONGODB_URL ??
-        'mongodb://cc_user:cc_password@localhost:27017/classconnect?authSource=classconnect',
-    ),
+    MongooseModule.forRoot(`${mongoUrl}/${mongoDb}`),
     AssessmentModule,
     MetricsModule,
   ],
